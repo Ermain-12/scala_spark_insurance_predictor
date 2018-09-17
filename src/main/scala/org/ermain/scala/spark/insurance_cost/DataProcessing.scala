3e50d1cba9f3011faea74441a0efd11861338699
Ermain <paulermain@gmail.com>
2018-09-16 21:29:22 -0400

    Beginning the feature engineering aspect of this part of the data analysis. Dropped one column("age") but I may need to drop some more columns and do some rescaling because of the imbalance in the data-set when comparing the charges based on the number of children each has. TODO: Try rescaling the dataset by number number of children each individual has so that there is a bit more balance within the dataset.

--- src/main/scala/org/ermain/scala/spark/insurance_cost/DataProcessing.scala
+++ src/main/scala/org/ermain/scala/spark/insurance_cost/DataProcessing.scala
@@ -21,19 +21,54 @@ object DataProcessing {
 
     inputData.show(10)
 
-    // Here, we group the sum charges by gender to check whether this data-set is balanced
+    /*
+    Here, we group the sum charges by gender to check whether this data-set is balanced
+                      +------+-----------------+
+                      |   sex|     sum(charges)|
+                      +------+-----------------+
+                      |  null|             null|
+                      |female|8321061.194618994|
+                      |  male|9434763.796139995|
+                      +------+-----------------+
+     */
     println("Showing charges by sex")
     inputData.groupBy("sex")
       .sum("charges")
       .show()
 
+    /*
+                  Group the charges by region:
+                  +---------+------------------+
+                  |   region|      sum(charges)|
+                  +---------+------------------+
+                  |northwest|4035711.9965399993|
+                  |southeast| 5363689.763290002|
+                  |northeast| 4343668.583308999|
+                  |     null|              null|
+                  |southwest| 4012754.647620001|
+                  +---------+------------------+
+     */
     // Group the charges by region
+
     println("Showing charges by region....")
     inputData.groupBy("region")
       .sum("charges")
       .show()
 
 
+    /*
+                +--------+------------------+
+                |children|      sum(charges)|
+                +--------+------------------+
+                |    null|              null|
+                |       1| 4124899.673449997|
+                |       3| 2410784.983589999|
+                |       5|      158148.63445|
+                |       4|346266.40777999995|
+                |       2| 3617655.296149999|
+                |       0| 7098069.995338997|
+                +--------+------------------+
+     */
     inputData.groupBy("children")
       .sum("charges")
       .show()
