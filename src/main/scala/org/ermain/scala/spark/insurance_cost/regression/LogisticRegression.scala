db6d7cc5a24dafcfac183e5679ab197d452ac021
Ermain <paulermain@gmail.com>
2018-09-16 23:30:53 -0400

    Created the pipeline to begin performing the regression and clustering analysis on the data-set. I will apply the algorithms on the data-set first, then I will rescale the features to determine if it provides a more accurate prediction.

--- src/main/scala/org/ermain/scala/spark/insurance_cost/regression/LogisticRegression.scala
+++ src/main/scala/org/ermain/scala/spark/insurance_cost/regression/LogisticRegression.scala
@@ -2,8 +2,10 @@ package org.ermain.scala.spark.insurance_cost.regression
 
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.sql.SparkSession
-import org.ermain.scala.spark.insurance_cost.{DataPipelineConstruction, SparkSessionCreate}
+import org.ermain.scala.spark.insurance_cost.{DataPipelineConstruction, DataProcessing, SparkSessionCreate}
 import org.ermain.scala.spark.insurance_cost.DataProcessing.trainingSet
 
 object LogisticRegression {
@@ -25,8 +27,6 @@ object LogisticRegression {
       .setLabelCol("label")
       .setFeaturesCol("features")
 
-
-
     // Chain indexers and tree in a Pipeline.
     val logRegressPipeLine = new Pipeline()
       .setStages(Array(
@@ -36,9 +36,37 @@ object LogisticRegression {
         logisticRegression
       ))
     // Fit the data to the regression model
-    val logisticRegressionModel = logRegressPipeLine
-        .fit(trainingSet)
+//    val logisticRegressionModel = logRegressPipeLine
+//        .fit(trainingSet)
 
+
+    // Search through decision tree's maxDepth parameter for best model
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(logisticRegression.maxIter, MaxIter)
+      .addGrid(logisticRegression.regParam, RegParam)
+      .addGrid(logisticRegression.tol, Tol)
+      .addGrid(logisticRegression.elasticNetParam, ElasticNetParam)
+      .build()
+
+    // Create the evaluator
+    val evaluator = new BinaryClassificationEvaluator()
+      .setLabelCol("label")
+      .setRawPredictionCol("prediction")
+
+    // Set up 10-fold cross validation
+    val crossValidator = new CrossValidator()
+      .setEstimator(logRegressPipeLine)
+      .setEvaluator(evaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(numFolds)
+
+    val crossValidModel = crossValidator.fit(trainingSet)
+
+    val predictions = crossValidModel.transform(DataProcessing.testingSet)
+    predictions.show()
+//    val result = predictions.select("label", "prediction", "probability")
+//    val resutDF = result.withColumnRenamed("prediction", "Predicted_label")
+//    resutDF.show(10)
 
   }
 }
